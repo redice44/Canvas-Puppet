ccf9af2aa839885ecbe2334f69392e9318bce0a4
Matt Thomson <red.cataclysm@gmail.com>
2017-10-27 11:54:57 -0400

    Login to Canvas LMS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import * as Puppeteer from 'puppeteer';
 
+import setupPage from './utility/setupPage';
+import login from './utility/login';
+
 run();
 
 async function run() {
   const browser: Puppeteer.Browser  = await Puppeteer.launch({
     // headless: false;
   });
-
+  const page: Puppeteer.Page = await setupPage(browser);
+  await login(page);
   browser.close();
 }
