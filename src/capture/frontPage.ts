5378c5101fc059b83440283a6bf6a13732062af1
Matt Thomson <red.cataclysm@gmail.com>
2017-10-30 16:40:34 -0400

    Captures canvas Pages

--- src/capture/frontPage.ts
+++ src/capture/frontPage.ts
@@ -4,18 +4,11 @@ import { rootUrl } from '../../private/lmsConfig';
 import { DeviceList } from '../stats/device';
 import { Course } from '../stats/course';
 import frontPageSelectors from '../config/selectors/frontPage';
-import goto from '../utility/goto';
-import emulateElement from '../utility/emulateElement';
-import ss from '../utility/ss';
+import capturePage from './page';
 
 export default async function captureFrontPage(page: Puppeteer.Page, course: Course, deviceList: DeviceList) {
   const courseUrl = `${rootUrl}/courses/${course.id}`;
   deviceList.screenshot.subPath = 'Front Page';
 
-  await goto(page, courseUrl);
-
-  for (const device of deviceList.devices) {
-    const content: Puppeteer.ElementHandle = await emulateElement(page, device.device, frontPageSelectors.content);
-    await ss(content, deviceList.screenshot, device.name);
-  }
+  await capturePage(page, courseUrl, frontPageSelectors.content, deviceList);
 }
---
+++ src/capture/page.ts
@@ -0,0 +1,15 @@
+import * as Puppeteer from 'puppeteer';
+
+import { DeviceList } from '../stats/device';
+import goto from '../utility/goto';
+import emulateElement from '../utility/emulateElement';
+import ss from '../utility/ss';
+
+export default async function capturePage(page: Puppeteer.Page, pageUrl: string, selector: string, deviceList: DeviceList) {
+  await goto(page, pageUrl);
+
+  for (const device of deviceList.devices) {
+    const content: Puppeteer.ElementHandle = await emulateElement(page, device.device, selector);
+    await ss(content, deviceList.screenshot, device.name);
+  }
+}
