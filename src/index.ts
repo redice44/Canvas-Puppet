ab8d4e40f3633a726a5c76d0e560545032f215ca
Matt Thomson <red.cataclysm@gmail.com>
2017-11-01 15:26:30 -0400

    Tests capturing a module

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,32 @@
 import * as Puppeteer from 'puppeteer';
 
-import loginModule from './admin/login';
-import courseListModule from './admin/courseList';
-import moduleListModule from './admin/moduleList';
-import frontPageModule from './capture/frontPage';
+import _login_ from './admin/login';
+import _courseList_ from './admin/courseList';
+import _moduleList_ from './admin/moduleList';
+import _frontPage_ from './capture/frontPage';
+import _captureModule_ from './capture/module';
 
 import { Course } from './interfaces/course';
 import { DeviceList } from './interfaces/device';
 import { LoginInfo } from './interfaces/credentials';
 import { ModuleItems } from './interfaces/module';
 
-
 export async function login(page: Puppeteer.Page, loginInfo: LoginInfo) {
-  await loginModule(page, loginInfo);
+  await _login_(page, loginInfo);
 }
 
 export async function courseList(page: Puppeteer.Page, rootUrl: string, includeTerms?: string[]): Promise<Course[]> {
-  return await courseListModule(page, rootUrl, includeTerms);
+  return await _courseList_(page, rootUrl, includeTerms);
 }
 
 export async function captureFrontPage(page: Puppeteer.Page, rootUrl: string, course: Course, deviceList: DeviceList) {
-  await frontPageModule(page, rootUrl, course, deviceList);
+  await _frontPage_(page, rootUrl, course, deviceList);
 }
 
 export async function moduleList(page: Puppeteer.Page, rootUrl: string, course: Course): Promise<ModuleItems[]> {
-  return await moduleListModule(page, rootUrl, course);
+  return await _moduleList_(page, rootUrl, course);
+}
+
+export async function captureModule(page: Puppeteer.Page, rootUrl: string, module: ModuleItems, deviceList: DeviceList) {
+  await _captureModule_(page, rootUrl, module, deviceList);
 }
